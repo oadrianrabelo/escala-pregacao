19e51c0f4d90f600352317573ff0f4a82b167f84
oadrianrabelo <adrianrabeloe10@gmail.com>
2026-02-11 16:13:36 -0400

    chore: configura layout, globals e dependências do projeto

---
+++ src/app/tech/page.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: "TechEduca",
+};
+
+export default function TechPage() {
+  return (
+    <iframe
+      src="/tech.html"
+      style={{
+        width: "100%",
+        height: "100vh",
+        border: "none",
+      }}
+    />
+  );
+}
