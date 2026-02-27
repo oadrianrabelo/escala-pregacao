5bc57ad017dadf5fa6471002619f2e70ae4bc1dc
oadrianrabelo <adrianrabeloe10@gmail.com>
2026-02-26 20:13:26 -0400

    fix: corrige `hydration mismatch` com `dynamic import`

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,10 @@
-import EscalaPregacao from "@/components/EscalaPregacao";
+"use client";
+
+import dynamic from "next/dynamic";
+
+const EscalaPregacao = dynamic(() => import("@/components/EscalaPregacao"), {
+  ssr: false,
+});
 
 export default function Home() {
   return <EscalaPregacao />;
